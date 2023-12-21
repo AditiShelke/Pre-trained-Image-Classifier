ac4b65113d3e448b719b01cca76c2d1d9af9cc30
Aditi Jaydeep Shelke <aditishelke333@gmail.com>
2023-12-21 20:00:02 +0530

    Add files via upload

---
+++ print_results.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/print_results.py
+#                                                                             
+# PROGRAMMER: 
+# DATE CREATED:
+# REVISED DATE: 
+# PURPOSE: Create a function print_results that prints the results statistics
+#          from the results statistics dictionary (results_stats_dic). It 
+#          should also allow the user to be able to print out cases of misclassified
+#          dogs and cases of misclassified breeds of dog using the Results 
+#          dictionary (results_dic).  
+#         This function inputs:
+#            -The results dictionary as results_dic within print_results 
+#             function and results for the function call within main.
+#            -The results statistics dictionary as results_stats_dic within 
+#             print_results function and results_stats for the function call within main.
+#            -The CNN model architecture as model wihtin print_results function
+#             and in_arg.arch for the function call within main. 
+#            -Prints Incorrectly Classified Dogs as print_incorrect_dogs within
+#             print_results function and set as either boolean value True or 
+#             False in the function call within main (defaults to False)
+#            -Prints Incorrectly Classified Breeds as print_incorrect_breed within
+#             print_results function and set as either boolean value True or 
+#             False in the function call within main (defaults to False)
+#         This function does not output anything other than printing a summary
+#         of the final results.
+##
+
+def print_results(results_dic, results_stats_dic, model, 
+                  print_incorrect_dogs = False, print_incorrect_breed = False):
+    print("\n\n*** Results Summary for CNN Model Architecture",model.upper(), 
+          "***")
+    print("{:20}: {:3d}".format('N Images', results_stats_dic['n_images']))
+    print("{:20}: {:3d}".format('N Dog Images', results_stats_dic['n_dogs_img']))
+    print("{:20}: {:3d}".format('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
+
+    # TODO: 6a. REPLACE print("") with CODE that prints the text string 
+    #          'N Not-Dog Images' and then the number of NOT-dog images 
+    #          that's accessed by key 'n_notdogs_img' using dictionary 
+    #          results_stats_dic
+    #
+ 
+
+
+    # Prints summary statistics (percent)
+    print("summarry model run: ")
+    for key in results_stats_dic:
+        print("\n".join(["{}: {}".format(key, results_stats_dic[key]) for key in results_stats_dic if
+                         key.startswith('p')]))
+        
+        #for key in results_stats_dic:
+        # if key[0] == 'p':
+        #print("{}: {}".format(key, results_stats_dic[key]))
+        # TODO: 6b. REPLACE pass with CODE that prints out all the percentages 
+        #           in the results_stats_dic dictionary. Recall that all 
+        #           percentages in results_stats_dic have 'keys' that start with 
+        #           the letter p. You will need to write a conditional 
+        #           statement that determines if the key starts with the letter
+        #           'p' and then you want to use a print statement to print 
+        #           both the key and the value. Remember the value is accessed 
+        #           by results_stats_dic[key]
+        #
+        
+
+
+    if (print_incorrect_dogs and 
+        ( (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'])
+          != results_stats_dic['n_images'] ) 
+       ):
+        print("\nINCORRECT Dog/NOT Dog Assignments:")     
+        for key in results_dic:
+            if (results_dic[key][3] == 1 and results_dic[key][4] == 0) or \
+            (results_dic[key][3] == 0 and results_dic[key][4] == 1):
+                print("Real: {}   Classifier: {}".format(results_dic[key][0],
+                                                         results_dic[key][1]))
+            
+
+    if (print_incorrect_breed and 
+        (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed']) 
+       ):
+        print("\nINCORRECT Dog Breed Assignment:")
+
+        for key in results_dic:
+            if ( sum(results_dic[key][3:]) == 2 and
+                results_dic[key][2] == 0 ):
+                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0],
+                                                          results_dic[key][1]))
